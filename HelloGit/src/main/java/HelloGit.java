c9801c2b3c77d8042b655d0723ad168167217b6a
Lee <272607704@qq.com>
2022-08-01 20:54:17 +0800

    提交文件

---
+++ HelloGit/src/main/java/HelloGit.java
@@ -0,0 +1,11 @@
+/**
+ * @author lee
+ * @create 2022-08-0120:40
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git!!!");
+
+    }
+}
